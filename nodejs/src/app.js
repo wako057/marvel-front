425bd17f21110c20742e27bedddfb993e972cc14
wako057 <wako057@gmail.com>
2019-07-01 22:11:54 +0200

    fix(lint): fix the lint

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -6,7 +6,6 @@ const port = process.env.NODE_PORT || 8080;
 const express = require("express");
 const app = express();
 
-
 const server = app.listen(port, () => {
     console.log(`
     Process ${process.pid} is listening to all incoming requests on port ${port},
