e8054290ac28267bcc95586377d17c34b1c909ae
wako057 <wako057@gmail.com>
2019-07-01 19:52:52 +0200

    feat(server): basic server listening to 8080

---
+++ nodejs/src/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// const env = process.env.NODE_ENV || "production";
+const port = process.env.NODE_PORT || 8080;
+
+const express = require("express");
+const app = express();
+
+
+const server = app.listen(port, () => {
+    console.log(`
+    Process ${process.pid} is listening to all incoming requests on port ${port},
+    workerProcess: ${process.pid}
+`);
+});
+
+module.exports = server;
