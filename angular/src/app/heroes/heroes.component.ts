02f909674e2822eba6ab93f5e548fdae2957e308
wako057 <wako057@gmail.com>
2019-07-02 22:29:38 +0200

    feat(angular): list mock heroes

--- angular/src/app/heroes/heroes.component.ts
+++ angular/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
 
   constructor() { }
 
