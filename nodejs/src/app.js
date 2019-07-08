a8585f17afd5a1ff0a95fccba45ecf25f00b2653
wako057 <wako057@gmail.com>
2019-07-08 02:27:40 +0200

    feat(node): use error handler & routes

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,19 +1,19 @@
 "use strict";
 
-const express = require("express")();
-const http = require("http").Server(express);
+const app = require("express")();
+const http = require("http").Server(app);
+const errorHandler = require("./errorHandler");
 
 const routes = require("./routes/index");
 
 const init = (config, loggers) => {
 
-    express.use(loggers.morganMiddleware); // Middleware to create access.log styled logs
-    express.use(loggers.loggerMiddleware); // Middleware to add logger in request object
-    express.use("/v1", routes());
+    app.use(loggers.morganMiddleware); // Middleware to create access.log styled logs
+    app.use(loggers.loggerMiddleware); // Middleware to add logger in request object
+    app.use("/v1", routes());
+    app.use(errorHandler); // Middleware to handle the errors
 
     return http;
 };
 
 module.exports = init;
-
-
