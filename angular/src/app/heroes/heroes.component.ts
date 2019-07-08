2e36f1322f9260caff3bfdedad9f3d1855d4f39b
wako057 <wako057@gmail.com>
2019-07-08 02:35:12 +0200

    feat(ng): upadte heroes component

--- angular/src/app/heroes/heroes.component.ts
+++ angular/src/app/heroes/heroes.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
+import {HttpClient} from "@angular/common/http";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
 
-  heroes = HEROES;
-
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
+    this.getHeroes()
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+
+    this.messageService.add(JSON.stringify(this.heroes));
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
