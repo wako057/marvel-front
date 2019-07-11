ca2289e4822e05cc364a2dd3042956b48e069947
Gregory Gerard <wako057@gmail.com>
2019-07-11 14:21:49 +0200

    feat(ng): add routing module

---
+++ angular/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent }      from './heroes/heroes.component';
+import { AboutComponent } from "./about/about.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'about', component: AboutComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
